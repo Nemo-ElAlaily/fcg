07db0643b9053d5d9b2591c8b4df1697544c3dda
Nemo-ElAlaily <tassnim.elalaily@gmail.com>
2024-03-10 08:11:07 -0500

    Updated the About slider image

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -4,17 +4,11 @@
 
 @section('hero')
 
-    <div class="hero-2 overlay" style="background-image: url('front/images/img_3.jpg');">
+    <div class="hero-2 overlay" style="background-image: url('front/images/about.png');">
 		<div class="container">
 			<div class="row align-items-center">
 				<div class="col-lg-5 mx-auto ">
 					<h1 class="mb-5 text-center"><span>About Us</span></h1>
-
-
-					<div class="intro-desc text-left">
-						<div class="line"></div>
-						<p>Delectus voluptatum distinctio quos eius excepturi sunt pariatur, aut, doloribus officia ea molestias beatae laudantium, quam odio ipsum veritatis est maiores velit quasi blanditiis et natus accusamus itaque.</p>
-					</div>
 				</div>
 			</div>
 		</div>
