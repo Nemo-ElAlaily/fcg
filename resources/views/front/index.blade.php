1254736714b259a5a6f485eafc15fa262b22e9a9
TassnimElAlaily <tassnim@whiteboxhr.com>
2024-03-28 14:52:23 +0200

    Change Latest projects height to be aligned

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -182,9 +182,9 @@
         <div class="row g-4">
             @foreach ($projects as $index => $project)
             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-                <div class="single-portfolio">
+                <div class="single-portfolio h-100">
                     <a href="{{ route('single.project', $project -> slug )}}">
-                        <img src="{{ $project -> image_path}}" alt="Image" class="img-fluid">
+                        <img src="{{ $project -> image_path}}" alt="Image" class="img-fluid h-100">
                         <div class="contents">
                             <h3>{{ $project -> title}}</h3>
                             <div class="cat">{{$project -> category ? $project->category->name : '' }}</div>
