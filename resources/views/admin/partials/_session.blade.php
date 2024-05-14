5e710757e7c96944d1c25a92025dfe06a4ef0250
TassnimElAlaily <tassnim@whiteboxhr.com>
2024-05-14 14:20:17 +0300

    Remove Unwanted Files

--- resources/views/admin/partials/_session.blade.php
+++
@@ -1,9 +0,0 @@
-@if (Session::has('success'))
-
-<div class="row mt-3">
-    <button type="text" class="text-sm  col-md-6 col-md-offset-3  btn successDelivered" id="type-error">{{
-        Session::get('success') }}
-    </button>
-</div>
-
-@endif
