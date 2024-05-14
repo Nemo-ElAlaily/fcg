c94e605d1affbab1f9ab7d39850264046d6f6234
TassnimElAlaily <tassnim@whiteboxhr.com>
2024-05-14 15:35:53 +0300

    Gallery modal and style

--- resources/views/front/single_project.blade.php
+++ resources/views/front/single_project.blade.php
@@ -1,14 +1,14 @@
 @extends('front.layouts.main')
 
-@section('title', $project -> title)
+@section('title', $project->title)
 
 @section('hero')
 
-<div class="hero-2 overlay slider-top" style="background-image: url('{{ $project -> image_path}}');">
+<div class="hero-2 overlay slider-top" style="background-image: url('{{ $project->image_path }}');">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-lg-5 mx-auto ">
-                <h1 class="mb-5 text-center"><span>{{ $project -> title }}</span></h1>
+                <h1 class="mb-5 text-center"><span>{{ $project->title }}</span></h1>
 
             </div>
         </div>
@@ -23,62 +23,61 @@
 
         <div class="row mb-5 justify-content-between">
             <div class="col-lg-4 mb-lg-0 mb-4">
-                <img src="{{ $project -> image_path }}" alt="Image" class="img-fluid w-100">
+                <img src="{{ $project->image_path }}" alt="Image" class="img-fluid w-100">
             </div>
             <div class="col-lg-8">
                 <!-- <div class="heading">Description</div>
-                <p>{!! $project -> description !!}</a></p> -->
+                    <p>{!! $project->description !!}</a></p> -->
                 <div class="row justify-content-between">
                     <div class="col-sm-4">
                         <span class="text-black-50 d-block">Scope of Services:</span>
                         <ul>
-                            @foreach($project -> services ->sortBy('created_at') as $index => $service)
-                            <span class="text-black-50 d-block"> {{ $service -> name }} </span>
+                            @foreach ($project->services->sortBy('created_at') as $index => $service)
+                            <span class="text-black-50 d-block"> {{ $service->name }} </span>
                             @endforeach
                         </ul>
                     </div>
                     <div class="col-sm-4 border-left">
                         <span class="text-black-50 d-block">Area:</span>
                         <ul>
-                            <span class="text-black-50 d-block"> {{ $project -> area }} </span>
+                            <span class="text-black-50 d-block"> {{ $project->area }} </span>
                         </ul>
                         <span class="text-black-50 d-block">Client:</span>
                         <ul>
-                            <span class="text-black-50 d-block"> {{ $project -> client -> name }} </span>
+                            <span class="text-black-50 d-block"> {{ $project->client->name }} </span>
                         </ul>
                         <span class="text-black-50 d-block">Location:</span>
                         <ul>
-                            <span class="text-black-50 d-block"> {{ $project -> location }} </span>
+                            <span class="text-black-50 d-block"> {{ $project->location }} </span>
                         </ul>
                         <span class="text-black-50 d-block">Category:</span>
                         <ul>
-                            <a
-                            href="{{route('category.projects', $project -> category -> slug)}}">{{ $project -> category
-                            -> name}}</a>
+                            <a href="{{ route('category.projects', $project->category->slug) }}">{{
+                                $project->category->name }}</a>
                         </ul>
 
                     </div>
-                    @if($project -> amenities || $project -> extra_info)
-                        <div class="col-sm-4 border-left">
-                            @if($project -> amenities)
-                            <div class="row mb-lg-0 mb-4">
-                                <span class="text-black-50 d-block">Amenities:</span>
-                                <ul>
-                                    <span class="text-black-50 d-block ml-4"> {!! $project -> amenities !!} </span>
-                                </ul>
-                            </div>
-                            @endif
-
-                            @if($project -> extra_info)
-                            <div class="row mb-lg-0 mb-4">
-                                <span class="text-black-50 d-block">Information:</span>
-                                <ul>
-                                    <span class="text-black-50 d-block ml-4"> {!! $project -> extra_info !!} </span>
-                                </ul>
-                            </div>
-                            @endif
+                    @if ($project->amenities || $project->extra_info)
+                    <div class="col-sm-4 border-left">
+                        @if ($project->amenities)
+                        <div class="row mb-lg-0 mb-4">
+                            <span class="text-black-50 d-block">Amenities:</span>
+                            <ul>
+                                <span class="text-black-50 d-block ml-4"> {!! $project->amenities !!} </span>
+                            </ul>
                         </div>
                         @endif
+
+                        @if ($project->extra_info)
+                        <div class="row mb-lg-0 mb-4">
+                            <span class="text-black-50 d-block">Information:</span>
+                            <ul>
+                                <span class="text-black-50 d-block ml-4"> {!! $project->extra_info !!} </span>
+                            </ul>
+                        </div>
+                        @endif
+                    </div>
+                    @endif
                 </div>
             </div>
         </div>
@@ -94,15 +93,52 @@
         </div>
 
         <div class="flex row">
-            @foreach(json_decode( $project -> gallery, true) as $index => $item)
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="py-2">
-                    <img src="{{ asset('uploads/projects/gallery/') . '/'. $item}}" alt="{{$project->title . $index }}"
-                        class="img-fluid">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 p-1">
+                @foreach (json_decode($project->gallery, true) as $index => $item)
+                @if ($index % 4 == 0)
+                <div class="py-1">
+                    <img src="{{ asset('uploads/projects/gallery/') . '/' . $item }}"
+                        alt="{{ $project->title . $index }}" class="img-fluid video-wrap glightbox" data-toggle="modal"
+                        data-target="#exampleModalCenter">
                 </div>
+                @endif
+                @endforeach
             </div>
-            @endforeach
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 p-1">
+                @foreach (json_decode($project->gallery, true) as $index => $item)
+                @if ($index % 4 == 1)
+                <div class="py-1">
+                    <img src="{{ asset('uploads/projects/gallery/') . '/' . $item }}"
+                        alt="{{ $project->title . $index }}" class="img-fluid video-wrap glightbox" data-toggle="modal"
+                        data-target="#exampleModalCenter">
+                </div>
+                @endif
+                @endforeach
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 p-1">
+                @foreach (json_decode($project->gallery, true) as $index => $item)
+                @if ($index % 4 == 2)
+                <div class="py-1">
+                    <img src="{{ asset('uploads/projects/gallery/') . '/' . $item }}"
+                        alt="{{ $project->title . $index }}" class="img-fluid video-wrap glightbox" data-toggle="modal"
+                        data-target="#exampleModalCenter">
+                </div>
+                @endif
+                @endforeach
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 p-1">
+                @foreach (json_decode($project->gallery, true) as $index => $item)
+                @if ($index % 4 == 3)
+                <div class="py-1">
+                    <img src="{{ asset('uploads/projects/gallery/') . '/' . $item }}"
+                        alt="{{ $project->title . $index }}" class="img-fluid video-wrap glightbox" data-toggle="modal"
+                        data-target="#exampleModalCenter">
+                </div>
+                @endif
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
+
 @endsection
